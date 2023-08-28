2a03936b496aca34cfd87f18e3779d3893069911
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-08-28 17:35:22 +0000

    Allow customization with args

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	if len(os.Args[1:]) > 0 {
+		name := os.Args[1]
+		fmt.Printf("O, hai %s!\n", name)
+		return
+	}
 	fmt.Println("O, hai!")
 }
