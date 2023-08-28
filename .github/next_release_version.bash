38b2eefeb0612dfd869de18ee551cc699a972b8b
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-08-28 20:18:34 +0000

    External script

---
+++ .github/next_release_version.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Set defaults
+next_version_release_year=$(TZ='UTC' date '+%Y')
+next_version_release_number=1
+
+tag=$(git describe --tags --match='release/[0-9][0-9][0-9][0-9].[0-9]*' refs/heads/main 2> /dev/null)
+regex='release\/([0-9]{4})\.([0-9]{0,})'
+if [[ $tag =~ $regex ]]; then
+    echo "Found tag for previous release: $tag"
+    prev_version_release_number="${BASH_REMATCH[2]}"
+    echo "Previous version number: $prev_version_release_number"
+    ((next_version_release_number=prev_version_release_number+1))
+else
+    echo "Could not locate a previous release version" >&2
+fi
+
+next_version="$next_version_release_year.$next_version_release_number"
+echo "Next version: $next_version"
+echo "result=$next_version" >> $GITHUB_OUTPUT
