c93dfa439ff7be2c3ed01258a974e45c67c21550
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-08-28 20:25:01 +0000

    External script debugging

--- .github/next_release_version.bash
+++ .github/next_release_version.bash
@@ -5,6 +5,7 @@ next_version_release_year=$(TZ='UTC' date '+%Y')
 next_version_release_number=1
 
 echo $GITHUB_REF
+echo "$(git describe --tags)"
 tag=$(git describe --tags --match='release/[0-9][0-9][0-9][0-9].[0-9]*' refs/heads/main)
 echo "got tag: $tag"
 regex='release\/([0-9]{4})\.([0-9]{0,})'
