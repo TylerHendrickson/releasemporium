08e341f76ad1cf7f23493197035767219bdc18b2
tyler <hendrickson.tsh@gmail.com>
2023-09-06 19:31:49 +0000

    Move script to workflow

--- terraform/script.bash
+++ terraform/script.bash
@@ -3,7 +3,7 @@
 # Exit if any steps fail
 set -e
 
-printf $(sh -c 'env' | base64 -w 0) > $HOME/some-output.txt
+# printf $(sh -c 'env' | base64 -w 0) > $HOME/some-output.txt
 
 mkdir -p ../bin
 go build -gcflags="-trimpath=$GOPATH" -asmflags="-trimpath=$GOPATH" -trimpath -ldflags=-buildid= -buildvcs=false -o ../bin/ohai ../cmd >&2 && echo '{}'
